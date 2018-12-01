036f25b6e83ee808e36f12e4fa2b84e51ffbc0ef
Joseph <joseph@ahnga.net>
2018-12-01 02:41:33 -0800

    Show right answer after user select the choice

--- assets/javacript/app.js
+++ assets/javacript/app.js
@@ -6,6 +6,16 @@ $(document).ready(function () {
     var startBtn = $("<button type='button' id='start' class='btn btn-danger hvr-pulse-grow'>CLICK HERE TO START</button>");
     screen.html(titleLogo).append(startBtn);
 
+    var initialOffset = 314;
+    var interval, interval2;
+    var clockRunning = false;
+    var correct = 0;
+    var wrong = 0;
+    var q; // question
+    var a; // answer
+    var qlist = [];
+    var qNum = 0;
+    var qEnd = 10;
     var qa = [
         ["ONE JUMP AHEAD OF THE __________,<br>ONE SKIP AHEAD OF MY DOOM", "Law-men", "Slowpokes", "Bread line", "Flock", 2],
         ["I KNOW YOU, THE GLEAM IN YOUR EYES<br>IS SO FAMILIAR A __________", "Stream", "Beam", "Dream", "Gleam", 4],
@@ -23,18 +33,8 @@ $(document).ready(function () {
         ["I'VE SEEN A FRONT PORCH SWING, HEARD A DIAMOND RING, I'VE SEEN A __________ __________ RAILROAD TIE", "Zig zag", "20 cent", "Bright blue", "Polka dot", 4],
         ["CAN YOU FEEL THE LOVE TONIGHT,<br>THE __________ THE EVENING BRINGS", "Peace", "Joy", "Rest", "Calm", 1]
     ];
-    var qlist = [];
 
-    var qNum = 0;
-    var qEnd = 10;
-    var q; // question
-    var a; // answer
-    var correct = 0;
-    var wrong = 0;
-    var initialOffset = 314;
-    var interval;
-    var clockRunning = false;
-
+    // Ramdomize questions' order
     for (var i = 0; i < qEnd; i++) {
         do {
             var num = Math.floor(Math.random() * 15);
@@ -43,66 +43,87 @@ $(document).ready(function () {
         qlist.push(num);
     }
 
-    startBtn.on("click", function () {
-        counter.css("display", "block");
-        timer.css("display", "block");
-        stopwatch.start();
-    });
-
+    // Game timer
     var stopwatch = {
         time: 0,
         endTime: 7,
-        i: 0,
+        qi: 0,
+        reset: function () {
+            clearInterval(interval2);
+            screen.empty();
+            q = qlist[qNum];
+            a = qa[q][5];
+            $('#timeNum').text(stopwatch.endTime);
+            var picture = $("<img>").attr({ "src": "./assets/images/" + q + ".jpeg" });
+            var question = $("<h5>").addClass("quiz bg-danger text-white ml-2 mr-2 p-3").html("♫ " + qa[q][0] + " ♫");
+            screen.append(picture, question);
+            for (var i = 1; i <= 4; i++) {
+                var answer = $("<button>").attr("id", "answer" + i).addClass("btn btn-outline-dark m-2 p-0 choices").css({ width: 132, height: 132 }).text(qa[q][i]).val(i);
+                screen.append(answer);
+            };
+        },
         start: function () {
-            quizOn();
-            $('.circle_animation').css('stroke-dashoffset', initialOffset - (0 * (initialOffset / stopwatch.endTime)));
+            stopwatch.reset();
             $('.circle_animation2').css('stroke-dashoffset', initialOffset - ((qNum + 1) * (initialOffset / qEnd)));
             $('#roundNum').text(qNum + 1);
-            i = 0;
-            stopwatch.time = 0;
+            stopwatch.qi = 0;
+            stopwatch.time = 1;
             if (!clockRunning) {
                 interval = setInterval(stopwatch.count, 1000);
                 clockRunning = true;
             }
         },
-        stop: function () {
-            clearInterval(interval);
-            clockRunning = false;
-            qNum++;
-            if (qNum < qEnd) {
-                stopwatch.start();
-            }
-        },
         count: function () {
-            $('#timeNum').text(stopwatch.endTime - i);
-            if (i == stopwatch.endTime) {
-                stopwatch.stop();
+            $('#timeNum').text(stopwatch.endTime - stopwatch.qi);
+            if (stopwatch.qi === stopwatch.endTime) {
+                stopwatch.stop(-1);
                 return;
             };
-            $('.circle_animation').css('stroke-dashoffset', initialOffset - ((i + 1) * (initialOffset / stopwatch.endTime)));
-            i++;
+            $('.circle_animation').css('stroke-dashoffset', initialOffset - ((stopwatch.qi + 1) * (initialOffset / stopwatch.endTime)));
+            stopwatch.qi++;
+        },
+        stop: function (answer) {
+            $('.circle_animation').css('stroke-dashoffset', initialOffset);
+            clearInterval(interval);
+            clockRunning = false;
+            if (answer === 1) {
+                // Correct answer
+                correct++;
+                $("#answer" + a).css({ "background-color": "#60bb60", color: "white" });
+            } else {
+                wrong++;
+                if (answer === 0) {
+                    // Wrong answer
+                    $("#answer" + a).css({ "background-color": "#d93749", color: "white" });
+                } else {
+                    // Time's up
+                    wrong++;
+                    $("#answer" + a).css({ "background-color": "#d93749", color: "white" });
+                };
+            };
+            if (++qNum < qEnd) {
+                interval2 = setInterval(stopwatch.start, 2500);
+            }
         }
-    }
-
-    function quizOn() {
-        screen.empty();
-        q = qlist[qNum];
-        a = qa[q][5];
-        var picture = $("<img>").attr({ "src": "./assets/images/" + q + ".jpeg", width: 579, height:330 });
-        var question = $("<h5>").addClass("quiz bg-danger text-white ml-2 mr-2 p-3").html("♫ " + qa[q][0] + " ♫");
-        screen.append(picture, question);
-        for (var i = 1; i <= 4; i++) {
-            var answer = $("<button>").addClass("btn btn-outline-dark m-2 p-0 choices").css({width:132, height:132}).text(qa[q][i]).val(i);
-            screen.append(answer);
-        };
     };
 
-    $(document).on("click",".choices", function(event) {
-        var choice = Number($(this).val());
-        if (a === choice) {
-            console.log("right");
-        } else {
-            console.log("wrong");
-        }
+    // if the start game button is clicked..
+    startBtn.on("click", function () {
+        counter.css("display", "block");
+        timer.css("display", "block");
+        $('.circle_animation').css('stroke-dashoffset', initialOffset);
+        stopwatch.start();
+    });
+
+    // if the answer button is clicked..
+    $(document).on("click", ".choices", function () {
+        if (clockRunning) {
+            var choice = Number($(this).val());
+            if (a === choice) {
+                stopwatch.stop(1);
+            } else {
+                stopwatch.stop(0);;
+            };
+        };
     });
 });
